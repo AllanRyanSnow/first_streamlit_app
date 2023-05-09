0bf255e009e2cd686f3702272d904535c3a22228
AllanRyanSnow <132936463+AllanRyanSnow@users.noreply.github.com>
2023-05-09 15:13:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,11 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected =  streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+# New Section to display Fruity Apps Menu Choices
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
 
 # Display the table on the page.
 
